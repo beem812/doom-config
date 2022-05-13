d2a3e573a00e99c8e040496de15a8e95f1d071d9
James Beem <beem132@gmail.com>
2022-05-13 10:46:28 -0400

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f00a605fb19cb258ad7e0d99c007f226f24d767d01bf31f3828ce6688cbdeb22" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "d516f1e3e5504c26b1123caa311476dc66d26d379539d12f9f4ed51f10629df3" "e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" "11cc65061e0a5410d6489af42f1d0f0478dbd181a9660f81a692ddc5f948bf34" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "249e100de137f516d56bcf2e98c1e3f9e1e8a6dce50726c974fa6838fbfcec6b" "06ed754b259cb54c30c658502f843937ff19f8b53597ac28577ec33bb084fa52" "9cd57dd6d61cdf4f6aef3102c4cc2cfc04f5884d4f40b2c90a866c9b6267f2b3" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "062e6ec918ed89d5d9a342dbbefd99e8690c5514c6698a78fc25f259972e9242" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" "1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "5b8311648b1d33cdf8732024e4a6f2e538a3a70ec3d328159116bb38038509c3" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
+ '(doom-gruvbox-brighter-comments nil t)
+ '(doom-zenburn-brighter-comments t)
+ '(doom-zenburn-brighter-modeline t)
+ '(doom-zenburn-comment-bg t)
+ '(org-agenda-files '("~/org/test.org" "/Users/jamesbeem/org/notes.org"))
+ '(package-selected-packages
+   '(kaolin-themes atom-one-dark-theme code-review forge solarized-theme cyberpunk-theme color-theme-sanityinc-solarized one-themes smart-mode-line-atom-one-dark-theme atom-dark-theme ancient-one-dark-theme color-theme-sanityinc-tomorrow @ jbeans-theme aggressive-indent)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2B2B2F" :foreground "#EEDCC1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 160 :width normal :foundry "nil" :family "MonoLisa"))))
+ '(font-lock-builtin-face ((t (:weight bold))))
+ '(font-lock-constant-face ((t (:foreground "#4FA8A3" :weight bold))))
+ '(font-lock-function-name-face ((t (:foreground "#32856e" :weight bold))))
+ '(font-lock-keyword-face ((t (:foreground "#4EB8CA" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "#fbaed2" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "#4FA8A3" :weight bold))))
+ '(font-lock-variable-name-face ((t (:foreground "#47ba99" :weight bold))))
+ '(hl-line ((t (:extend t :background "#303035" :weight bold))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#4FA8A3" :weight bold))))
+ '(scala-font-lock:abstract-face ((t (:inherit font-lock-builtin-face :weight bold)))))
